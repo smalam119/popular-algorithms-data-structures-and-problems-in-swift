0d3933e2c66a0a2bd89b0956b89ef0f396579a77
smalam119 <smalam119@gmail.com>
2018-07-27 02:23:01 +0600

    binary search func name error solved

--- Algorithms/Search/BinarySearch.swift
+++ Algorithms/Search/BinarySearch.swift
@@ -3,13 +3,13 @@
 
 struct BinarySearch {
 	
-	func searchRecursion(key: Int, array: [Int], start: Int, end: Int) -> Int {
+	func search(key: Int, array: [Int], start: Int, end: Int) -> Int {
 		var mid = start + (end - start) / 2
 		if(start < end) {
 			if(key > array[mid]) {
-				return binarySearchRecursion(key: key,array: array,start: mid + 1,end: end)
+				return search(key: key,array: array,start: mid + 1,end: end)
 			} else if(key < array[mid]) {
-				return binarySearchRecursion(key: key,array: array ,start: start,end: mid)
+				return search(key: key,array: array ,start: start,end: mid)
 			} else {
 				return mid
 			}
