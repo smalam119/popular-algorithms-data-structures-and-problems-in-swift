eb66da9a8a050f24a425a2044106f465e3d6852c
Sayed Mahmudul Alam <smalam119@gmail.com>
2023-04-15 18:08:16 +0800

    update array rotate to in place

--- ProblemSolving/ArrayRotate.swift
+++ ProblemSolving/ArrayRotate.swift
@@ -1,20 +1,46 @@
-//Array Rotate
-//by Sayed Mahmudul Alam
+// Author: Sayed Mahmudul Alam
+// Source: LeetCode-189: https://leetcode.com/problems/rotate-array/description/
+// Example Input: nums = [1,2,3,4,5,6,7], k = 3
+// Output: [5,6,7,1,2,3,4]
 
 struct ArrayRotate {
-	
-	func rotate(array: [Int], numberOfRotation: Int) -> [Int] {
-		var tempArray = [Int]()
-		let r = numberOfRotation % array.count
+    func rotate(array: [Int], numberOfRotation: Int) -> [Int] {var tempArray = [Int]()
+        let r = numberOfRotation % array.count
 		
-		for i in r..<array.count {
-			tempArray.append(array[i])
-		}
-		
-		for i in 0..<r {
-			tempArray.append(array[i])
-		}
-		
-		return tempArray
+	for i in r..<array.count {
+	    tempArray.append(array[i])
 	}
+		
+	for i in 0..<r {
+	    tempArray.append(array[i])
+	}
+		
+	return tempArray
+     }
+}
+
+class ArrayRotateInPlace {
+	
+    func rotate(_ nums: inout [Int], _ k: Int) {
+
+        let r = k % nums.count
+        
+        reverse(&nums, from: 0, to: nums.count - 1)
+        reverse(&nums, from: 0, to: r - 1)
+        reverse(&nums, from: r, to: nums.count - 1)
+    }
+
+    func reverse(_ nums: inout [Int], from start: Int, to end: Int) {
+        var s = start
+        var e = end
+
+        while(s < e) {
+            let temp = nums[s]
+            nums[s] = nums[e]
+            nums[e] = temp
+
+            s += 1
+            e -= 1
+        }
+    }
 }
