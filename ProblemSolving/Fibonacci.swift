16d9b5c55dd2a58508eff392e966130d382c6e33
Sayed Mahmudul Alam <smalam119@gmail.com>
2023-04-15 15:48:54 +0800

    Add DP solution

--- ProblemSolving/Fibonacci.swift
+++ ProblemSolving/Fibonacci.swift
@@ -5,7 +5,7 @@
 // Output: 2
 // Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
 
-class Solution {
+class RecursiveSolution {
     func fib(_ n: Int) -> Int {
         if n == 0 {
             return 0
@@ -16,5 +16,31 @@ class Solution {
         }
 
         return fib(n-1) + fib(n-2)
+    }
+}
+
+class DPSolution {
+    func fib(_ n: Int) -> Int {
+
+        guard n > 1 else {
+            return n
+        }
+
+        // The first two values of the series are 0 and 1
+        var first = 0
+        var second = 1
+
+        // Since first two values are known, the loops can be started from 2
+        for i in 2...n {
+
+            // current term is the sum of previous two values
+            let term = first + second
+
+            // Now first term is second term and second term is the current term
+            first = second
+            second = term
+        }
+
+        return second
     }
 }
